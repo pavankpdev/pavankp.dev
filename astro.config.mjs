1609def66a6f4f55dfa27f4cfecaaa6edc897907
pavanleton <pavankumarkpkp@gmail.com>
2023-05-09 00:12:43 +0530

    udpate: add social icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  css: [
-    "./src/styles/fonts.css", // path to your CSS file
+  css: ["./src/styles/fonts.css" // path to your CSS file
   ],
+  integrations: [react()]
 });
