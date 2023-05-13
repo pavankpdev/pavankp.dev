af1defbe4ead76e07351065d6862c04d292250a8
pavanleton <pavankumarkpkp@gmail.com>
2023-05-13 13:42:34 +0530

    update: list blogs

---
+++ src/data/index.ts
@@ -0,0 +1,26 @@
+export const writings = [
+  {
+    id: 1,
+    title: "The Ultimate Deep Dive into React Server Components (Revised)",
+    description: `React Server Components have been something that's been bugging me for a while now. There's been a lot of debate online, and a lot of comparisons have made it really hard to understand what exactly it is and how it works. But now, after doing some re...`,
+    url: "https://blogs.pavankp.dev/the-ultimate-deep-dive-into-react-server-components-revised",
+  },
+  {
+    id: 2,
+    title: "Become a React Pro: Top 10 Tips for Improving Your Skills",
+    description: `React is a popular JavaScript library widely used for building web applications. Whether you are a beginner or an experienced developer, there is always room for improvement when it comes to React skills...`,
+    url: "https://blogs.pavankp.dev/become-a-react-pro-top-10-tips-for-improving-your-skills",
+  },
+  {
+    id: 3,
+    title: "Co-Locating State: Learn to Optimize React Apps without Memo",
+    description: `As the application grows, React states become more widespread. Poorly organized states can cause excessive re-rendering and hinder performance. To address this problem, React developers often resort to optimizing it using memoization without thoroughly exa...`,
+    url: "https://blogs.pavankp.dev/co-locating-state-learn-to-optimize-react-apps-without-memo",
+  },
+  {
+    id: 4,
+    title: "Does Re-rendering in React Affect Performance",
+    description: `A significant amount of time is often spent trying to optimize a React app. Though optimization is a crucial phase in software development, it can go too far. Let's discuss Re-renders in React today and whether they impact performance...`,
+    url: "https://blogs.pavankp.dev/does-re-rendering-in-react-affect-performance",
+  },
+];
