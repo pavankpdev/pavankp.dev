7adbc0ca73e65a323509659ccfc678b80a253901
Pavan Kumar <52795191+pavankpdev@users.noreply.github.com>
2023-10-04 13:18:21 +0530

    Update projects

--- src/data/index.ts
+++ src/data/index.ts
@@ -36,24 +36,24 @@ export const works = [
   },
   {
     id: 2,
+    title: "GoAA",
+    description: `An Account Abstraction (ERC-4337) package written in go.`,
+    repo: "https://github.com/pavankpdev/goaa",
+    opensource: true,
+  },
+  {
+    id: 3,
     title: "DocMySol",
     description: `Docmysol is a user-friendly solution for creating Solidity documents with just one click.`,
     url: "https://docmysol.com/",
     sideProject: true,
   },
   {
-    id: 3,
+    id: 4,
     title: "SicaPay",
     description: `Fight Climate Change And Earn Rewards With SICA. A Digital Currency Backed By Real Carbon Assets.`,
     url: "https://app.sicapay.io/auth/login",
     freelance: true,
-  },
-  {
-    id: 4,
-    title: "ArtPlace",
-    description: `An open-source exclusive NFT marketplace that connects artists with collectors, providing a platform for unique and rare digital art ownership.`,
-    repo: "https://github.com/DuolearnHQ/ArtPlace",
-    opensource: true,
   },
 ];
 
