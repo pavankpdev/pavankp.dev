8ff03bb9bdd2a557966306ce030752a32be2ca70
Pavan Kumar <52795191+pavankpdev@users.noreply.github.com>
2024-04-23 13:10:14 +0530

    Update Resume

--- src/components/SocialGridIcons.tsx
+++ src/components/SocialGridIcons.tsx
@@ -38,7 +38,7 @@ const SocialIconsDeck = () => {
         })}
         <a 
         className="icon__item view__resume__btn"
-         href={`https://pavan-public.s3.ap-south-1.amazonaws.com/Pavan's+Resume+detailed.pdf`} 
+         href={`https://pavan-public.s3.ap-south-1.amazonaws.com/Pavan's+Resume.pdf`} 
          target="_blank"
          >
             View Resume
